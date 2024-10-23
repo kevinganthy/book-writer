8a6df8520fc185821b3873453bab71463afc092f
Kevin <kevin.ganthy@oclock.io>
2024-10-23 16:20:03 +0000

    typo

--- src/components/Toc.svelte
+++ src/components/Toc.svelte
@@ -23,7 +23,7 @@
             {#each $content as item}
                 {#if item.type === "bloc" }
                     <li class="px-2 border-l flex flex-col gap-2">
-                        <buttonclass="button button-ghost text-left" on:click={() => goTo(item.id)}>
+                        <button class="button button-ghost text-left" on:click={() => goTo(item.id)}>
                             <p class="line-clamp-1">{item.value}</p>
                             <div class="flex gap-2">
                                 {#each item.tags ?? [] as tag}
